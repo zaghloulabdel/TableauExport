6930165500151955ecd6bfb738f688fc9324defd
zaghloulabdel <54242393+zaghloulabdel@users.noreply.github.com>
2025-02-24 14:04:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     tableau.extensions.initializeAsync().then(() => {
         console.log("Extension Tableau initialisée !");
-    }).catch(err => {
-        console.error("Erreur lors de l'initialisation :", err);
+        
+        document.getElementById("export-btn").addEventListener("click", () => {
+            alert("Export en cours !");
+        });
     });
 });
